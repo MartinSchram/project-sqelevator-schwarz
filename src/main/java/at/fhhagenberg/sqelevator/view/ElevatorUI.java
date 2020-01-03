b8754c55805ae725d320884391a09e665886a2c1
polato <onurpolat1994@gmail.com>
2020-01-03 16:40:49 +0100

    creating first UI and executing with javafx:run

---
+++ src/main/java/at/fhhagenberg/sqelevator/view/ElevatorUI.java
@@ -0,0 +1,30 @@
+
+package at.fhhagenberg.sqelevator.view;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class ElevatorUI implements Initializable{
+
+	
+	@FXML
+	public Label lbA;
+	public Label lbB;
+	public Label lbC;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	 
+	
+}
