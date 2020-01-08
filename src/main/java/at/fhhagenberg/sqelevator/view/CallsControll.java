0ed1ec62373fc5c9c4b86eebd33de6b973d36ba4
s1810567018@students.fh-hagenberg.at <s1810567018@students.fh-hagenberg.at>
2020-01-08 11:25:21 +0100

    added robot-test skeleton

--- src/main/java/at/fhhagenberg/sqelevator/view/CallsControll.java
+++ src/main/java/at/fhhagenberg/sqelevator/view/CallsControll.java
@@ -19,5 +19,4 @@ public class CallsControll extends HBox {
         }
     }
 
-
 }
---
+++ src/test/java/ElevatorUiTest.java
@@ -0,0 +1,31 @@
+/*package at.fhhagenberg.sqelevator;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import javafx.stage.Stage;
+
+@ExtendWith(ApplicationExtension.class)
+public class ElevatorUIApp {
+	@Start
+	public void start(Stage stage) throws Exception {
+		new ElevatorUIApp().start(stage);
+	}
+
+	@Test
+	public void testCall(FxRobot robot) {
+		robot.doubleClickOn("tbtnOpModeAuto");
+		robot.doubleClickOn("#b").write("2");
+
+		robot.clickOn("#compute");
+
+		verifyThat(".label.sum", hasText("3.00"));
+	}
+}*/
