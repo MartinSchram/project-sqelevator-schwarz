a057ee61995fa34dcb2864d28ff5ed1e602c54a2
s1810567018@students.fh-hagenberg.at <s1810567018@students.fh-hagenberg.at>
2020-02-11 13:28:39 +0100

    refactored

--- src/main/java/at/fhhagenberg/sqelevator/model/ElevatorActions.java
+++ src/main/java/at/fhhagenberg/sqelevator/model/ElevatorActions.java
@@ -1,62 +1,53 @@
 package at.fhhagenberg.sqelevator.model;
 
 /**
- * @author Martin Schram
- * Container for elevator actions.
- *
+ * @author Martin Schram Container for elevator actions.
  */
 public class ElevatorActions {
-
 
 	/**
 	 * default-ctor for Actions to be set onto the elevator on a cycle
 	 */
-	public ElevatorActions() {		
+	public ElevatorActions() {
 	}
-	
+
 	/**
-	 * ctor with init of all members for Actions to be set onto the elevator on a cycle
+	 * ctor with init of all members for Actions to be set onto the elevator on a
+	 * cycle
 	 */
-	public ElevatorActions(int target, boolean commitedDirIsUp, boolean isAuto, boolean[] servicedFloors) {	
-		this.TargetFl=target;
-		this.CommitedDirIsUp =commitedDirIsUp;
-		this.AutoMode=isAuto;
-		this.ServicesFloors=servicedFloors;
+	public ElevatorActions(int target, boolean commitedDirIsUp, boolean isAuto, boolean[] servicedFloors) {
+		this.TargetFl = target;
+		this.CommitedDirIsUp = commitedDirIsUp;
+		this.AutoMode = isAuto;
+		this.ServicesFloors = servicedFloors;
 	}
-	
+
 	/**
 	 * Clone to get shallow copy.
 	 */
 	@Override
-    protected ElevatorActions clone() throws CloneNotSupportedException {
-        return (ElevatorActions)super.clone();
-    }
-	
+	protected ElevatorActions clone() throws CloneNotSupportedException {
+		return (ElevatorActions) super.clone();
+	}
+
 	/**
-	 * Set the Targetfloor
-	 * Property to be set by gui-binding
+	 * Set the Targetfloor Property to be set by gui-binding
 	 */
 	public int TargetFl;
-	
+
 	/**
-	 * Set the ComitedDir
-	 * Property to be set by gui-binding
+	 * Set the ComitedDir Property to be set by gui-binding
 	 */
 	public boolean CommitedDirIsUp;
-	
+
 	/**
-	 * Set Automode
-	 * Property to be set by gui-binding
+	 * Set Automode Property to be set by gui-binding
 	 */
 	public boolean AutoMode;
-	
-	
+
 	/**
-	 * Set Servicefloors directly
-	 * Property to be set by gui-binding
+	 * Set Servicefloors directly Property to be set by gui-binding
 	 */
 	public boolean[] ServicesFloors;
-	
-	
-	
+
 }
