ced5a987a933d19cc3708307f1c627a3c088824b
s1810567018@students.fh-hagenberg.at <s1810567018@students.fh-hagenberg.at>
2020-02-11 08:02:32 +0100

    refactored main

--- src/main/java/at/fhhagenberg/sqelevator/MainAppClass.java
+++ src/main/java/at/fhhagenberg/sqelevator/MainAppClass.java
@@ -26,16 +26,6 @@ public class MainAppClass extends Application {
 	public static void main(String[] args) {
 
 		launch();
-//		// init gui
-//		// init bindings
-//		//
-//		int ELEVATORSCOUNT =3;
-//		int FLOORS = 5;
-//		ElevatorController ev = new ElevatorController(ELEVATORSCOUNT,FLOORS);
-//		// init bindings -> for(i<ELEVATORSCOUNT) ElevatorController.GuiActions[i] = gui binding
-//		ev.init(null);
-//		// ev.addObserver(GUI);
-//		// new thread start: ev.RunCyclic();
 
 	}
 
